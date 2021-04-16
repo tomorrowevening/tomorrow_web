45117ad036fcd483e401b7e5e905365d17082e73
Colin Duffy <colin@tomorrowevening.com>
2021-04-16 16:06:47 -0700

    0.0.961

---
+++ dist/index.d.ts
@@ -0,0 +1,12 @@
+import debug from './debug';
+export { debug };
+import { isiPad, isiPhone, isiOS, isChrome, isSafari, isFirefox, isOpera, isMobile, hasUserMedia, supportsWebp, delay, fileName, guid } from './dom';
+export { isiPad, isiPhone, isiOS, isChrome, isSafari, isFirefox, isOpera, isMobile, hasUserMedia, supportsWebp, delay, fileName, guid };
+import loader from './loader';
+export { loader };
+import { DEGREES, RADIANS, TWO_PI, HALF_PI, between, clamp, normalize, mix, map, roundTo, distance, distance2, distance3, random, getAngle, toRad, toDeg, cubicBezier, mixArrays } from './math';
+export { DEGREES, RADIANS, TWO_PI, HALF_PI, between, clamp, normalize, mix, map, roundTo, distance, distance2, distance3, random, getAngle, toRad, toDeg, cubicBezier, mixArrays };
+import raf from './raf';
+export { raf };
+import { orthoCamera, plane, dispose, parseShader, anchorGeometry, anchorGeometryTL, setBlendNormal, setBlendAdd, setBlendMultiply, setBlendScreen, FBO, DoubleFBO, Pass, updateTextureData } from './three';
+export { orthoCamera, plane, dispose, parseShader, anchorGeometry, anchorGeometryTL, setBlendNormal, setBlendAdd, setBlendMultiply, setBlendScreen, FBO, DoubleFBO, Pass, updateTextureData };
